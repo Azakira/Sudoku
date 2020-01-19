cfeae8fd6c7fa13a4656bf39b3e9928ef6cdde86
2chenj <habinshutithierry@gmail.com>
2020-01-19 18:02:24 +0100

    fonction file_to_string added to the project

--- sudoku.ml
+++ sudoku.ml
@@ -9,12 +9,21 @@ let initGrille str =
 	let mat = make_matrix 9 9 0 in
 	for i=0 to 9 do 
 		for j=0 to 9 do 
-			mat.(i).(j) <- get str.(j);
+			mat.(i).(j) <- int_of_char str.(j);
+                        
 		done
 	done;
 	mat
 ;;
-
+(*
+let affiche_matrix mat =
+        for i=0 to mat.length do
+                for j=0 to mat.length do
+                        print_int mat.(i).(j);
+                done
+        done
+;;*)
+                        
 let ()=
 	let file = open_in path in
 	try
@@ -25,3 +34,19 @@ let ()=
 	with e ->                      (* some unexpected exception occurs *)
 	  close_in_noerr file;           (* emergency closing *)
 	  raise e
+;;
+
+let file_to_string path =
+        let file = open_in path in
+        try 
+                let line = input_line file in
+                close_in file;
+          line;
+        with e->
+                close_in_noerr file;
+                raise e
+;;
+
+print_endline " affiche du resulat de file_to_string";;
+print_string (file_to_string path) ;;
+
