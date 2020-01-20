e16d800576162c49185cd44c30d5aae59e611a24
Azakira <47180416+Azakira@users.noreply.github.com>
2020-01-20 15:37:15 +0100

    Prints matrix in cmd

--- sudoku.ml
+++ sudoku.ml
@@ -3,27 +3,34 @@ open Printf
 open String
 open Array
 
-(* let path = "/mnt/c/Users/nizef/Documents/THIERRY/WorkSpace/L3INFO/Sudoku/grids/grid0.txt" *)
-let path = "/Users/ishnuts/Documents/GitHub/Sudoku/grids/grid0.txt"
+let path = "/mnt/d/Documents/Github/Sudoku/grids/grid0.txt"
+(*let path = "/mnt/c/Users/nizef/Documents/THIERRY/WorkSpace/L3INFO/Sudoku/grids/grid0.txt"*)
+(* let path = "/Users/ishnuts/Documents/GitHub/Sudoku/grids/grid0.txt" *)
 let initGrille str =
-	let mat = make_matrix 9 9 0 in
-	for i=0 to 9 do 
-		for j=0 to 9 do 
-			mat.(i).(j) <- int_of_char str.(j);
-                        
-		done
+	let mat = make_matrix 9 9 '0' in
+	for i=0 to 8 do
+		begin
+			for j=0 to 8 do
+				mat.(i).(j) <- str.[i*9+j]
+			done;
+		end
 	done;
 	mat
-;;
-(*
-let affiche_matrix mat =
-        for i=0 to mat.length do
-                for j=0 to mat.length do
-                        print_int mat.(i).(j);
-                done
-        done
-;;*)
-                        
+
+
+let affiche_grille mat =
+	for i=0 to 8 do
+	 begin
+		for j=0 to 8 do
+			if mat.(i).(j)='0' then print_string "." else print_char mat.(i).(j);
+			if j=2 || j=5 || j=8 then print_string "|" else	print_string " ";
+		done;
+		print_string "\n";
+		if i=2 || i=5 || i=8 then print_string "─────────────────\n";
+	 end
+	done
+
+
 let file_to_string path =
         let file = open_in path in
         try 
@@ -33,10 +40,11 @@ let file_to_string path =
         with e->
                 close_in_noerr file;
                 raise e
-;;
+
 
 let file_string = file_to_string path;;
 print_endline " affiche du resulat de file_to_string ";;
-print_endline (file_string) ;;
+let g = initGrille file_string;;
+affiche_grille g;;
 
  
