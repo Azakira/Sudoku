c26e4058a34ffc5dff2684d1fcd9605f2e026aca
2chenj <habinshutithierry@gmail.com>
2020-01-28 19:22:52 +0100

    can we verifiy all cases ? yes no  maybe i dunno

--- sudoku.ml
+++ sudoku.ml
@@ -6,6 +6,7 @@ open Array
 
 (*let path = "/mnt/d/Documents/Github/Sudoku/grids/grid0.txt"*)
 let path = "/home/tochange/Documents/L3INFO/ocaml/Sudoku/grids/grid0.txt"
+let pathS ="/home/tochange/Documents/L3INFO/ocaml/Sudoku/solutions/solution0.txt" 
 (* let path = "/Users/ishnuts/Documents/GitHub/Sudoku/grids/grid0.txt" *)
 let initGrille str =
 	let mat = make_matrix 9 9 '0' in
@@ -43,14 +44,6 @@ let file_to_string path =
                 raise e
 
 ;;
-let file_string = file_to_string path;;
-print_endline " affiche du resulat de file_to_string ";;
-let g = initGrille file_string;;
-affiche_grille g;;
-let  rec loop () = 
-        loop()
-;;
-
 let draw_sudoku  width height  =
         for i=0 to height-90 do
                for j =0 to width-90 do
@@ -75,8 +68,60 @@ let stroke_that_bitch width height =
 	done;
 	draw_rect 0 0 width  height;
 ;;
-		 
-	
+
+let caseCorrect x y grid soln = 
+        if grid.(x).(y)==soln.(x).(y) then
+               (* print_endline "you got this "*)
+                true 
+        else 
+                (*print_endline "tony is that you ? "*)
+                false
+;;
+
+let insertValueInMatrix x y valeur mat = 
+        mat.(x).(y) <- valeur;
+        mat
+;;
+let removeValueOfMatrix x y mat = 
+        mat.(x).(y) <- '0';
+        mat
+;;
+
+let comp arg1 arg2 =
+        let rec compare l1 l2 = 
+                match l1 with
+                [] ->true
+                |h::t-> if h!= List.hd l2 return false compare t (List.tl l2)
+        in compare arg1 arg2
+;;
+
+let verifGrille matRep matSol = 
+        let listRep = to_list matRep in
+        let listSol = to_list matSol in
+        compare listRep listSol
+;;
+
+(* MAIN*)
+let file_string = file_to_string path;;
+print_endline " affiche du resulat de file_to_string ";;
+let grilleReponse= initGrille file_string;;
+
+let file_stringSolution = file_to_string pathS;;
+let grilleSolution = initGrille file_stringSolution;;
+
+grilleReponse = insertValueInMatrix 0 0 '8' grilleReponse;;
+
+caseCorrect 0 0 grilleReponse grilleSolution;;
+affiche_grille grilleReponse;;
+
+removeValueOfMatrix 0 0 grilleReponse;;
+print_endline " apres suppression de la valuer";
+affiche_grille grilleReponse;;
+
+verifGrille grilleReponse grilleSolution;;
+let  rec loop () = 
+        loop()
+;;
 let () = 
         open_graph " 811x811";
         set_window_title " sudoku ";
