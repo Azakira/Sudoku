9be6c0d90418762cb75990f49d9e66f4877863f9
2chenj <habinshutithierry@gmail.com>
2020-03-07 16:11:43 +0100

    documentation that may or may not be usefull !

--- sudoku.ml
+++ sudoku.ml
@@ -6,16 +6,26 @@ open Array
 (*PATH ZAK*)
 (*let path = "/mnt/d/Documents/Github/Sudoku/grids/grid0.txt"*)
 
-
 (*PATH*)
-let path = "/mnt/c/Users/student/Documents/THIERRY/WorkSpace/L3INFO/Sudoku/grids/grid0.txt"
+let pathReponse = "/home/tochange/Documents/thierry/workSpace/psud/l3Info/s6/pfa/Sudoku/grids/grid0.txt"
+let pathSolution = "/home/tochange/Documents/thierry/workSpace/psud/l3Info/s6/pfa/Sudoku/solutions/solution0.txt"
 (*let pathS ="/home/tochange/Documents/L3INFO/ocaml/Sudoku/solutions/solution0.txt"*)
+
 
 (*PATH TONY*)
 (*let pathS ="/Users/ishnuts/Documents/GitHub/Sudoku/solutions/solution0.txt"*)
 (* let path = "/Users/ishnuts/Documents/GitHub/Sudoku/grids/grid0.txt" *)
 (*let path = "/home/tp-home007/tabemon/L3/S6/PFA/Sudoku-master/grids/grid0.txt"*)
 
+
+(*function initGrille
+  given  a string (one line = unidimension) big enough here 9*9 =81 characters, considering 
+  a nested loop of i and j where i are lines and j colomns (0<=i<=8 and 0<=j<=8)
+  return a matrix where the values at (i,j) is a  couple of the value at
+  i*9+j of the string and false if the value is '0', true otherwise
+  @param  a string of integers
+  @return a 9*9 matrix of couples (int as a char, bool)
+*)
 let initGrille str =
         let mat = make_matrix 9 9 ('0',true) in
 	for i=0 to 8 do
@@ -29,6 +39,11 @@ let initGrille str =
 	mat
 
 ;;
+
+(*procedure affiche_grille  
+  @param a 9*9 matrix of couples (int as a char, bool)
+  @return none
+*)
 let affiche_grille mat =
 	for i=0 to 8 do
 		for j=0 to 8 do
@@ -50,6 +65,14 @@ let affiche_grille mat =
         else print_string "not ok";
 
 ;;
+
+(*function file_to_string 
+  given  a file/system/path to a file, if it can not open the file return an error
+  otherwise this  function open the file
+  grab the content as string with the function input_line and return the string
+  @param a file system path
+  @return a string
+*)
 let file_to_string path =
         let file = open_in path in
         try 
@@ -61,6 +84,15 @@ let file_to_string path =
                 raise e
 
 ;;
+
+
+(*procedure draw_sudoku
+  given a height and a width, considering considering a nested loop where i are horizental lines ans j
+  vertical lines, i and j varying from 0 to (height|width/90)-1=8 as  we  choose height and width  to get 9*9 cellulors
+  and we draw lines every 90 pixel  (i*90)(j*90)
+  @param a width, a height
+  @return none
+*)
 let draw_sudoku  width height  =
         for i=0 to (height/90)-1 do
                for j =0 to (width/90)-1 do
@@ -69,7 +101,13 @@ let draw_sudoku  width height  =
         done;
 ;;
 
-
+(*function stroke 
+  given a width and height will chossen as we want 9*9 grid
+  considering a nested loop i and j from 0 to 3 car height(810)/270 =3
+  stroke lines at every 270 pixel stroke on length of 270 (3*width(810)/9)
+  @param a width and height
+  @return none
+*)
 let stroke width height =
 
 	for i=0 to height/270 do
@@ -81,46 +119,116 @@ let stroke width height =
 	draw_rect 0 0 width  height;
 ;;
 
-
+(*function insertValueInMatrix 
+  given a x, a y, a value: interger as a char  and a matrix, this fct put the value in the
+  position (x,y) of the matrix a,d return the matrix
+  @param x, y : integers for the position, and an Interger as char and a matrix of couples (integer as a char, bool)
+  @return a matrix of couples (integer as a char, bool)
+*)
 let insertValueInMatrix x y valeur mat = 
-        mat.(x).(y) <- valeur;
+        mat.(x).(y) <- (valeur,true);
         mat
 ;;
+
+(*function removeValueOfMatrix 
+  given a x, a y and a matrix of couples (integer as a char, bool)
+  put the value at (x,y) of the matrix to '0'(zero as a char)
+  @param x, y integers and a matrix of couples (integer as a char, bool)
+  @return matrix of couples (integer as a char, bool)
+*)
 let removeValueOfMatrix x y mat = 
         mat.(x).(y) <- '0';
         mat
 ;;
 
+(*bool compare
+  given 2 lists, this function compare one element of one list to one element of other list 
+  that are at the same position 
+  @param l1 ,l2: Lists
+  @return bool
+*)
+let rec compare l1 l2 = 
+        match l1 with
+        |[] -> true
+        |h::t-> if h!= List.hd l2 then false else compare t (List.tl l2)
+;;
+
+(*function verifGrille
+  given 2 matrix, this fct  transforms every matrix into a list, then call
+  the function compare on those 2 lists if compare return true this fct 
+  return "c bon" return "c pas bon" otherwise
+  @param matRep, matSol : matrix of couples (integer as a char, bool)
+  @return a string
+*)
+let verifGrille matRep matSol = 
+        let listRep = to_list matRep in
+        let listSol = to_list matSol in
+        if(compare listSol listRep== true) then print_endline "c bon" else print_endline " c pas bon"
+;;
+
+(*procedure  afficheMousPos
+  given a position, print its coordinate separatly 
+  @param a position : couple (x,y)
+  @return none
+*)
+let afficheMousPos pos =
+  print_string "\n x : " ;
+  print_int (fst pos);
+  print_string " \n y: ;";
+  print_int (snd pos); 
+;;
+
+(*function loop
+  calls its self
+  @param unit
+  @return none
+*)
 let  rec loop () = 
         loop()
 ;;
-(*let () = 
+
+(*unit
+  function calls int that unit are done on the execution of this file
+  @param none 
+  @return none
+*)
+let () = 
         open_graph " 811x811";
         set_window_title " sudoku ";
         draw_sudoku 810 810;
 	stroke 810 810;
         sound 10 10 ;
+        print_int (Graphics.current_x());
         loop ()
+
 ;;
-*)
+
  
                
 
 
 (* MAIN *)
 
-let file_string = file_to_string path;;
-print_endline " affiche du resulat de file_to_string ";;
-let grilleReponse= initGrille file_string;;
+(* import grid*)
+let file_stringR = file_to_string pathReponse;;
+(* import solution*)
+let file_stringS = file_to_string pathSolution;;
 
-grilleReponse = insertValueInMatrix 0 0 ('8',true) grilleReponse;;
-grilleReponse = insertValueInMatrix 0 1 ('3',true) grilleReponse;;
-grilleReponse = insertValueInMatrix 0 2 ('5',true) grilleReponse;;
+(*make grille of grid*)
+let grilleReponse= initGrille file_stringR;;
+
+(*make grille of solution*)
+let grilleSolution = initGrille file_stringS;;
+
+
+grilleReponse = insertValueInMatrix 0 0 '8' grilleReponse;;
+grilleReponse = insertValueInMatrix 0 1 '3' grilleReponse;;
+grilleReponse = insertValueInMatrix 0 2 '5' grilleReponse;;
 
 affiche_grille grilleReponse;;
 
+verifGrille grilleSolution grilleSolution;;
 
-affiche_grille grilleReponse;;
 
 (*
 
